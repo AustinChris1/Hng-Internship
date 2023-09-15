f9ccf7be0923c3974d41006638156e3e4c2a080a
AustinChris1 <austinchrisiwu@gmail.com>
2023-09-15 02:53:13 +0100

    fuck env

--- stage2/db.php
+++ stage2/db.php
@@ -1,8 +1,8 @@
 <?php 
-$db_host = getenv('DB_HOST');
-$db_user = getenv('DB_USER');
-$db_password = getenv('DB_PASSWORD');
-$db_name = getenv('DB_NAME');
+$db_host="sql.freedb.tech";
+$db_user="freedb_austin";
+$db_password="Zu9X$&HS#uk&vhV";
+$db_name="freedb_austinpersons";
 	$db = new mysqli($db_host, $db_user, $db_password, $db_name) or die('error with connection');
 ?>
 
